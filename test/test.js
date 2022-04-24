598c68f3e6454221afc763c264d7df9bf59b900e
berrouayel djamila <102658745+berrouayeldjamila@users.noreply.github.com>
2022-04-24 17:11:51 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 describe('Get a course', function(){
 var course;
-fetch("https://syntaxmapbackmain.herokuapp.com/" + "PRESENT SIMPLE").then(res =>res.json())
+fetch("https://syntaxmapfrontmain.herokuapp.com/" + "PRESENT SIMPLE").then(res =>res.json())
 .then((res) => {course = res[0].course_title});
     it('should work', function(){
         assert.equal(course, "PRESENT SIMPLE");
